e95a761bc6fd394bf946c3648856150bde0c9946
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-04-07 10:12:30 +0200

    modification et insertion compteur txt

--- fichier.php
+++ fichier.php
@@ -1,8 +1,13 @@
 <?php
 
+//Mise à jour//
 $file = fopen("miseajour.txt", "c");
 
-fwrite($file, " La mise a jour à étais faite le:");
+fwrite($file, " La mise a jour à étais faite le: ".PHP_EOL);
+
+//Compteur//
+$file = fopen("compteur.txt", "c");
+
 
 var_dump(fgets($file));
 
