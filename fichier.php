de6b900ec4a323d812581ee0312d87cda3b625db
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-04-07 09:53:36 +0200

    modification

--- fichier.php
+++ fichier.php
@@ -2,6 +2,9 @@
 
 $file = fopen("miseajour.txt", "c");
 
+fwrite()
+
+//La date et l'heure//
 $date = date("d-m-Y");
 $heure = date("H:i");
 Print("la dernière mise à jour à était faite le $date à $heure");
