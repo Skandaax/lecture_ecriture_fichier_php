db4765f9c3c6f60a9395210875e957f278ad8651
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-04-07 10:55:12 +0200

    modification

--- fichier.php
+++ fichier.php
@@ -6,7 +6,28 @@ $file = fopen("miseajour.txt", "c");
 fwrite($file, " La mise a jour à étais faite le: ".PHP_EOL);
 
 //Compteur//
-$file = fopen("compteur.txt", "c");
+if(file_exists("compteur.txt"))
+{
+    $compteur_f = fopen('"compteur.txt", "c"');
+    $compte = fgets($compteur_f);
+}else
+{
+    $compteur_f = fopen('compteur.txt', "c");
+    $compte = 0;
+}
+
+if(!isset($session['compteur_page']))
+{
+    $session['compteur_page'] = 'visite';
+    $compte++;
+    fseek($compteur_f, 0);
+    fputs($compteur_f, $compte);
+}
+
+fclose($compteur_f);
+echo '<strong>' .$compte. '</strong> visites.';
+
+
 
 
 var_dump(fgets($file));
